c33f0d1aaa4eb52e020edeb9d2340827d25afaf0
Gleb Smirnov <Gleb_Smirnov@epam.com>
2017-11-20 11:02:25 +0300

    test dev branch

--- src/Sort.java
+++ src/Sort.java
@@ -12,6 +12,7 @@ public class Sort {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
+					System.out.println("method bubleSort");
                 }
             }
         }
