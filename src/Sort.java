49a37b10e1d724ebfee3ac7ddeb90e07e40dfb3c
Uladzimir Shyrei <Uladzimir_Shyrei@epam.com>
2017-11-20 11:48:14 +0300

    Vse slomali

--- src/Sort.java
+++ src/Sort.java
@@ -2,16 +2,7 @@ class Sort {
 
     static void bubbleSort(int[] arr) {
 
-        for (int i = arr.length - 1; i > 0; i--) {
+        System.out.println("Pizda methody");
 
-            for (int j = 0; j < i; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
     }
 }
