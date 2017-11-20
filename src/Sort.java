e0a26b4348535929dcb5385dfbe230aefca3e5ff
Uladzimir Shyrei <Uladzimir_Shyrei@epam.com>
2017-11-20 11:59:20 +0300

    Final task

--- src/Sort.java
+++ src/Sort.java
@@ -4,7 +4,16 @@ class Sort {
 
     static void bubbleSort(int[] arr) {
 
-        System.out.println("Pizda methody");
+        for (int i = arr.length - 1; i > 0; i--) {
 
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
     }
 }
