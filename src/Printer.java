a03238c04631dd86eceb89d033dcee4bbecfc400
Kseniya Pashchuk <Kseniya_Pashchuk@epam.com>
2017-11-21 14:18:18 +0300

    test

--- src/Printer.java
+++ src/Printer.java
@@ -2,7 +2,7 @@ class Printer {
 
     static void print(int[] arr){
         for (int elem: arr) {
-            System.out.println(elem);
+            System.out.println(elem + "bla-bla");
         }
     }
 }
